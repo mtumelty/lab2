514455620b88270ce3e781c68de154072eda9c07
mtumelty <m186606@usna.edu>
2018-02-01 10:39:26 -0500

    upload for meesh

---
+++ game.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import serial
+import pygame
+from pygame.locals import *
+import sys
+
+
+while(True):
+	draw_wold(window)
+	for event in pygame.event.get():
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_UP:
+				my_launcher.changeAngle(3)
+			#TODO: handle rest of direction changes
+		if event.type == QUIT:
+			pygame.quit()
+			sys.exit()
+	my_launcher.draw(window)
+	pygame.display.update()
+	fpsClock.tick(FPS)
+
