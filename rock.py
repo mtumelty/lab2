37552a16fed3fa7725f343e5c6fe6a346abcd588
Bush <mtbush118@gmail.com>
2018-02-07 22:55:55 -0500

    adding colors and rocks

---
+++ rock.py
@@ -0,0 +1,28 @@
+import pygame
+from colors import *
+
+class Rock:
+  def __init__(self, x,y):
+    self.x = x
+    self.y= y
+    self.v_x = 0
+    self.v_y = 0
+    pass
+  def move(self, time):
+    self.x= self.x + self.v_x*time
+    self.y= self.y - self.v_y*time
+    #TODO
+    pass
+  def isMoving(self):
+    if (self.v_x != 0) or (self.v_y != 0):
+      return True
+    else:
+      return False
+   
+  def draw(self, surf):
+    r= pygame.Rect((0,0,10,10))
+    r.center= (self.x, self.y)
+
+    pygame.draw.rect(surf, ROCK_COLOR, r)
+  
+
