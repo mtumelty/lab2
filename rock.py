5209e2771edd0e517531b4a473878410bcb808c4
mtumelty <m186606@usna.edu>
2018-02-12 10:24:38 -0500

    post lab scrubbing

--- rock.py
+++ rock.py
@@ -15,23 +15,27 @@ class Rock:
     #move in y
     if(self.y<400):
       self.v_y = self.v_y - G*time
-      self.y= self.y - self.v_y*time
+    self.y= self.y - self.v_y*time
 
-    #TODO
-    pass
+  def moveTo(self, x, y):
+   	self.x = x
+   	self.y = y
+   	self.v_x = 0
+   	self.v_y - 0
+
   def isMoving(self):
     if (self.v_x != 0) or (self.v_y != 0):
       return True
     else:
       return False
    
-	def draw(self, surf):
+  def draw(self, surf):
 		self.r= pygame.Rect((0,0,10,10))
 		self.r.center= (self.x, self.y)
 
-		pygame.draw.rect(surf, ROCK_COLOR, r)
+		pygame.draw.rect(surf, ROCK_COLOR, self.r)
 	
-	def getRect(self):
+  def getRect(self):
 		return self.r
     
   
